fa6b69a22ae09bef59c6256d34c534347440e507
Cristian Pachon <cristianpachon@Cristians-MacBook-Pro.local>
2020-03-08 12:05:23 +0100

    adding new files

--- notebooks/cpachon/first_model_bug.py
+++ notebooks/cpachon/first_model_bug.py
@@ -1,13 +1,13 @@
 import tensorflow.keras
 from unet import unet_model_3d
 import os
-
+from notebooks.cpachon.data import get_train_and_validation_datasets
 
 input_shape = (1,40,40,40)
 model = unet_model_3d(input_shape = input_shape, n_labels = 1, activation_name = 'relu')
 #model.summary()
 
-from notebooks.cpachon.data import get_train_and_validation_datasets
+
 
 batch_size = 2
 path_images = os.path.join(os.getcwd(), "data", "resized_dataset", "imagesTr")
@@ -28,8 +28,8 @@ train_dataset, validation_dataset, validation_images = get_train_and_validation_
 train_dataset = train_dataset.shuffle(100).batch(batch_size)
 validation_dataset = validation_dataset.batch(batch_size)
 
-train_dataset = train_dataset.batch(batch_size)
-validation_dataset = validation_dataset.batch(batch_size)
+#train_dataset = train_dataset.batch(batch_size)
+#validation_dataset = validation_dataset.batch(batch_size)
 
 for image in train_dataset:
     x,y = image
