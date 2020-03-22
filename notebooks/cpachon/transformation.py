86b76a57ad0c1347631bb5925afda6d06a617ffa
cristianpachon <jupyter@roger.europe-west2-a.c.freelearing.internal>
2020-03-22 09:53:50 +0000

    Documentation for transformations

--- notebooks/cpachon/transformation.py
+++ notebooks/cpachon/transformation.py
@@ -1,5 +1,16 @@
 def decide_to_apply(cond):
-    decision = cond and np.random.random(1) > -1
+    
+    """
+    Decide whether to apply or not a condition
+    
+    Args:
+        cond (bool): condition to be applied
+    
+    Returns:
+        bool: whether to apply the input condition or not
+    """
+    
+    decision = cond and np.random.random(1) > 0.5
     return decision
 
 
@@ -9,6 +20,20 @@ def random_flip(
     apply_flip_axis_x, 
     apply_flip_axis_y, 
     apply_flip_axis_z):
+    
+    """
+    Randomly flip spatial dimensions
+    
+    Args:
+        image (np.ndarray): image to be flipped
+        target (np.ndarray): target to be flipped
+        apply_flip_axis_x (bool): whether to flip x axis or not
+        apply_flip_axis_y (bool): whether to flip y axis or not
+        apply_flip_axis_z (bool): whether to flip z axis or not
+    
+    Returns:
+        tuple: image and target flipped
+    """
     
     apply_flip = [apply_flip_axis_x, apply_flip_axis_y, apply_flip_axis_z]
 
@@ -23,6 +48,19 @@ def random_flip(
 
 def add_gaussian_offset(image, apply_gaussian_offset, sigma):
     
+    """
+    Add Gaussian offset to an image. Adds the offset to each channel
+    independently.
+    
+    Args:
+        image (np.ndarray): image to add noise to
+        apply_gaussian_offset (bool): whether to add Gaussian offset or not
+        sigma (float): stddev of the Gaussian distribution to generate noise from
+    
+    Returns:
+        np.ndarray: same as image but with added offset to each channel
+    """
+    
     if decide_to_apply(apply_gaussian_offset):
         print("Applying gaussian offset")
         offsets = np.random.normal(0, sigma, ([1] * (image.ndim - 1) + [image.shape[-1]]))
@@ -32,6 +70,18 @@ def add_gaussian_offset(image, apply_gaussian_offset, sigma):
 
 
 def add_gaussian_noise(image, apply_gaussian_noise, sigma):
+    
+    """
+    Add Gaussian noise to an image
+    
+    Args:
+        image (np.ndarray): image to add noise to
+        apply_gaussian_noise (bool): whether to add Gaussian noise or not
+        sigma (float): stddev of the Gaussian distribution to generate noise from
+    
+    Returns:
+        np.ndarray: same as image but with added offset to each channel
+    """
  
     if decide_to_apply(apply_gaussian_noise):
         print("Applying noise")
@@ -41,6 +91,27 @@ def add_gaussian_noise(image, apply_gaussian_noise, sigma):
     return image
 
 def elastic_transform(image, target, apply_elastic_transfor, alpha, sigma):
+    
+    """
+    
+    Elastic deformation of images as described in [1].
+    [1] Simard, Steinkraus and Platt, "Best Practices for Convolutional
+        Neural Networks applied to Visual Document Analysis", in Proc. of the
+        International Conference on Document Analysis and Recognition, 2003.
+    Based on gist https://gist.github.com/erniejunior/601cdf56d2b424757de5
+    
+    Args:
+        image (np.ndarray): image to be deformed
+        target (np.ndarray) : target to be deformed
+        apply_elastic_transfor (bool): whether to apply elastic transformation or not
+        alpha (list): scale of transformation for each dimension, where larger
+            values have more deformation
+        sigma (list): Gaussian window of deformation for each dimension, where
+            smaller values have more localised deformation
+    
+    Returns:
+        tuple: deformed image and deformed target
+    """
 
     assert len(alpha) == len(sigma), "Dimensions of alpha and sigma are different for elastic transform"
     
@@ -98,6 +169,29 @@ def apply_transformations(
     alpha_elastic = [0,0,0],
     sigma_elastic = [1,1,1] 
 ):
+    
+    """
+    Apply some transformation in order to have data augmentation
+    
+    Args:
+        image (np.ndarray): image to apply transformations to
+        target (np.ndarray): target to apply transformations to
+        apply_flip_axis_x (bool): whether to flip x axis or not
+        apply_flip_axis_y (bool): whether to flip y axis or not
+        apply_flip_axis_z (bool): whether to flip z axis or not
+        apply_gaussian_offset (bool): whether to add Gaussian offset or not
+        apply_gaussian_noise (bool): whether to add Gaussian noise or not
+        apply_elastic_transfor (bool): whether to apply elastic transformation or not
+        sigma_gaussian_offset (bool): stddev of the Gaussian distribution to generate noise from
+        sigma_gaussian_noise (bool): stddev of the Gaussian distribution to generate noise from
+        alpha_elastic (bool): scale of transformation for each dimension, where larger
+            values have more deformation
+        sigma_elastic (bool): Gaussian window of deformation for each dimension, where
+            smaller values have more localised deformation
+    
+    Returns:
+        tuple: image and target with the transformation applied
+    """
     assert image.shape == target.shape, "Image and target have different shapes"
     
     image = image.astype(float)
